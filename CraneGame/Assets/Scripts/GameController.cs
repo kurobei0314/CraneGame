2533833ab781ae127da72629c2253a58eb94021a
kurobei0314 <maikurodei.biseibutu@gmail.com>
2021-05-01 10:54:11 +0900

    ボタンの点滅の実装

--- CraneGame/Assets/Scripts/GameController.cs
+++ CraneGame/Assets/Scripts/GameController.cs
@@ -5,6 +5,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UniRx;
+using System;
 
 public class GameController : MonoBehaviour
 {
@@ -12,12 +14,36 @@ public class GameController : MonoBehaviour
     public GameObject AMButton;
     int FallButtonFlg = 0;
 
+    private float angularFrequency = 5f;
+    static readonly float DeltaTime = 0.0333f;
+
     // Start is called before the first frame update
     void Start()
     {
-        //GameObject RButton = AMButton.transform.Find("Right").gameObject;
-        //GameObject LButton = AMButton.transform.Find("Left").gameObject;
-    
+        GameObject RButton = AMButton.transform.Find("Right").gameObject;
+        GameObject LButton = AMButton.transform.Find("Left").gameObject;
+        GameObject FButton = AMButton.transform.Find("Fall").gameObject;
+
+        float time = 0.0f;
+
+        Observable.Interval(TimeSpan.FromSeconds(DeltaTime)).Subscribe(_ =>
+        {
+            
+            time += angularFrequency * DeltaTime;
+            var Rcolor = RButton.GetComponent<Image>().color; 
+            var Lcolor = LButton.GetComponent<Image>().color;
+            var Fcolor = FButton.GetComponent<Image>().color;
+
+            Rcolor.a = Mathf.Sin(time) * 0.5f + 0.5f;
+            Lcolor.a = Mathf.Sin(time) * 0.5f + 0.5f;
+            Fcolor.a = Mathf.Sin(time) * 0.5f + 0.5f;
+
+            RButton.GetComponent<Image>().color = Rcolor; 
+            LButton.GetComponent<Image>().color = Lcolor;
+            FButton.GetComponent<Image>().color = Fcolor;
+
+        }).AddTo(this);
+
         //ボタンの動作
         //RButton.GetComponent<Button>().onClick.AddListener(RButtonTouch);
         //LButton.GetComponent<Button>().onClick.AddListener(LButtonTouch);
